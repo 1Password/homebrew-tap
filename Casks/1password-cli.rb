412f7303eae2ffee107c9e04e89baab801363788
AndyTitu <andi.titu@agilebits.com>
2023-08-31 12:32:10 +0200

    Download op zip and place it in homebrew's bin

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,15 +1,16 @@
 cask "1password-cli" do
-  version "2.20.0"
-  sha256 "932b405de96fbcff9205a9e21e5a3d2532b52cb46b79f80bb963e10bcb02ed4f"
+  arch arm: "arm64", intel: "amd64"
 
-  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_apple_universal_v#{version}.pkg"
+  version "2.20.0"
+  sha256 arm:   "6df48caac5bc09e4e4690c272e5013cba3f24e8e6524133a8d92dcf40ca8d65f",
+         intel: "d0481587937ad80fff251c5636df0506b69bf7efe3a83478f1df9132b7c02408"
+
+  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_darwin_#{arch}_v#{version}.zip"
   name "1Password CLI"
   desc "Command-line interface for 1Password"
   homepage "https://developer.1password.com/docs/cli"
 
-  pkg "op_apple_universal_v#{version}.pkg"
+  binary "op"
 
-  uninstall pkgutil: "com.1password.op"
-
-  zap trash: "~/.op"
+  # No zap stanza required
 end
