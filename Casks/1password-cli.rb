abd47ea2e4159d532539d97dcfe8b442bf52b02b
Jody Heavener <jody.heavener@agilebits.com>
2026-02-05 11:02:22 -0700

    Update homebrew-tap to v2.32.1

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,9 +1,9 @@
 cask "1password-cli" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2.30.3"
-  sha256 arm:   "0472133a09a0596119543c0ee7def0b3c08646354432516a18dbfe831d536c88",
-         intel: "79fa39079b038bfefa8dafbc9e6cd1540ad869532ddbb6cf0a0b829edc7f4d48"
+  version "2.32.1"
+  sha256 arm:   "63310eb6a66dedbc940d6fa80faab4a07f68d54a7add91236dfb60f748bbac2d",
+         intel: "876017f048449c972b891715c2dbb30ca7d6d3929d398b1c6d0b19eeb75627f1"
 
   url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_darwin_#{arch}_v#{version}.zip"
   name "1Password CLI"
