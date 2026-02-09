afcfb2291b869822877baea1879631a1199a81f3
Horia Culea <45151601+hculea@users.noreply.github.com>
2026-02-09 11:22:12 +0100

    Update SHA256 checksums for 1Password CLI
    These have been set to the wrong versions priorly.

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -2,8 +2,8 @@ cask "1password-cli" do
   arch arm: "arm64", intel: "amd64"
 
   version "2.32.1"
-  sha256 arm:   "63310eb6a66dedbc940d6fa80faab4a07f68d54a7add91236dfb60f748bbac2d",
-         intel: "876017f048449c972b891715c2dbb30ca7d6d3929d398b1c6d0b19eeb75627f1"
+  sha256 arm:   "fcf1038da5d1030c87e3382553a875638f17e19204874bed0034382ce2dfd4b8",
+         intel: "6469797129d9e70129aac1828893a53ef535be3c7d4bd1e235498b8d78d6a4eb"
 
   url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_darwin_#{arch}_v#{version}.zip"
   name "1Password CLI"
