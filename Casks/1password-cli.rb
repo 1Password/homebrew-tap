3f4007dad397762e94305c4374c3a900558e6c2c
zack kourouma <zack.kourouma@agilebits.com>
2026-04-16 17:49:09 -0400

    feat: Upgrade to v2.34.0

--- Casks/1password-cli.rb
+++ Casks/1password-cli.rb
@@ -1,9 +1,9 @@
 cask "1password-cli" do
   arch arm: "arm64", intel: "amd64"
 
-  version "2.33.1"
-  sha256 arm:   "8106127624bf55f0bf5ae6f8e2c56398e01db75d64c3088eface33885c854351",
-         intel: "4ac94e529b93525d85c58d5a608a514444036897338e65208df9c84113140f94"
+  version "2.34.0"
+  sha256 arm:   "a9c93d45ad62665c6350ed8958f281a94034a79c94e88077e530f6d129699a5d",
+         intel: "d2e4cbdf8b5d2d421a411e5251c6956f3ffce90adde82b2a429435645c9bc91d"
 
   url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_darwin_#{arch}_v#{version}.zip"
   name "1Password CLI"
